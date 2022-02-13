92c5ca40fe67dba7be0aec9e657381b3b7e2fcda
Sapporo Jones <sapporojones@localhost.localdomain>
2022-02-12 16:30:29 -0800

    fixed wordwrap issue in detail-view

--- sappastebin/settings.py
+++ sappastebin/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(-bvsys68h9yo^o!yodvuc&&t2sl-vr)#@5p)bsvjw12me+*vh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
